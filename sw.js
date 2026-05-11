2b463742d5e8e5f72247325a1ea951cedb6bfa0c
yukimiyumama <arihappyyoko3@gmail.com>
2026-05-11 20:00:38 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE = 'pca-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'pca-v3';
+const ASSETS = [
+  '/kakaku-app/',
+  '/kakaku-app/index.html',
+  '/kakaku-app/manifest.json',
+  '/kakaku-app/icon-192.png',
+  '/kakaku-app/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -8,13 +14,13 @@ self.addEventListener('install', e => {
 
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/kakaku-app/index.html')))
   );
 });
